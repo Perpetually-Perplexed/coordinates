4e0017c09eb6ea4a94ebae3addaa59f6abee72a9
BuildTools <unconfigured@null.spigotmc.org>
2021-11-27 10:23:33 +0530

    Moved Coordinates to CoordinateUtils

--- src/main/java/me/perplexed/coordinates/Main.java
+++ src/main/java/me/perplexed/coordinates/Main.java
@@ -1,21 +1,18 @@
 package me.perplexed.coordinates;
 
-import me.perplexed.coordinates.utils.Coordinate;
-import me.perplexed.coordinates.utils.CoordinateSaveFile;
-import org.bukkit.Location;
+import me.perplexed.coordinates.coordinate.Coordinate;
+import me.perplexed.coordinates.coordinate.CoordinateSaveFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public final class Main extends JavaPlugin {
 
     private final List<Coordinate> coords  =new ArrayList<>();
     private static Main inst;
     private final CoordinateSaveFile saveFile = new CoordinateSaveFile(this);
-    public boolean leoLychee = false; // ignore this in public releases
+    public final boolean leoLychee = false; // ignore this in public releases
 
 
     @Override
@@ -26,16 +23,9 @@ public final class Main extends JavaPlugin {
         getCommand("coordinates").setExecutor(new MegaCommand());
     }
 
-    public CoordinateSaveFile getSaveFile() {
-        return saveFile;
-    }
 
     public static Main getInstance() {
         return inst;
-    }
-
-    public List<Coordinate> getCoords() {
-        return coords;
     }
 
     @Override
